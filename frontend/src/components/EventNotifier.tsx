599cb1536f52ed83ad18237c57edf010e1e614c4
Ramizalam <ramizalam63@gmail.com>
2026-02-16 17:27:08 +0530

    added Event Notification

--- frontend/src/components/EventNotifier.tsx
+++ frontend/src/components/EventNotifier.tsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
+import store from '../store'
+import Button from './Button';
+import { roundService } from '../service/RoundServices';
+import Notification from './Notification';
+import { observer } from 'mobx-react-lite';
 
-const EventNotifier = () => {
+interface Props{
+
+}
+
+
+const EventNotifier : React.FC<Props> = () => {
+
+  const { currentPlayerId,choosing,myChance,wordList,currentWord} = store.gameStore;
+
+  const drawer = store.gameStore.getPlayerById(currentPlayerId!);
+
+
+  // first show the 4 words to the drawer and if drawer click one of the word to will emit an event to other user that the game has Started and other will see _ _ _ _ on their website
+
+  const memorizedWord = useMemo(()=>{
+       return wordList.map((word)=>(
+        <Button onClick={()=>roundService.roundSyncClient(word)}
+        key={word}
+        >
+         {word} 
+       </Button>
+       ))
+  },[myChance,choosing,wordList])
+
+  const [notifySelection,setNotifySelection] = useState(false);
+  const[wordReveal , setWordReveal]  = useState(false);
+
+    useEffect(()=>{
+      if(myChance || !drawer)  return;
+        setNotifySelection(true);
+        setTimeout(()=>{
+          setNotifySelection(false);
+        },2000)
+      
+    },[choosing,myChance,drawer])
+
+
+    useEffect(()=>{
+      if(myChance || choosing) return;
+
+      if(currentWord){
+        setWordReveal(true);
+        setTimeout(()=>{
+          setWordReveal(false);
+        },2000)
+      }
+    },[myChance,currentWord])
+
+
   return (
-    <div>EventNotifier</div>
+    // this will be seen by the drawer 
+    <>
+    <Notification open ={choosing && myChance && !currentWord} >
+        <div className='flex flex-col items-center space-y-4'>
+        <h2>Choose a Word</h2>
+        {memorizedWord}
+        </div>
+      </Notification>  
+
+       <Notification open={wordReveal}>
+        <h2>Word was {currentWord}</h2>
+      </Notification>
+
+        {/* this will be seen by  other player */}
+      <Notification open={notifySelection} >  
+        <>
+        {choosing?(
+          <span>
+            <span  className='text-green-700 '>{drawer?.name}</span> is choosing a word
+          </span>
+        ):(
+          <span>
+            <span className="text-green-700">{drawer?.name}</span> starts
+              drawing
+          </span>
+        )}
+        
+        </>
+      </Notification>
+    
+    </>
   )
 }
 
-export default EventNotifier
+export default observer(EventNotifier);
--- frontend/src/components/Player.tsx
+++ frontend/src/components/Player.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import store from '../store'
+import { GameStateEnum } from '../enums/GameState';
+import Avatar from './Avatar';
+import avatarImage from '../assets/avatarImage.png'
+import { observer } from 'mobx-react-lite';
 
-const Player = () => {
+interface  Props{}
+
+const Player : React.FC<Props> = (props) => {
+
+  const{topScorers:players,gameState} = store.gameStore;
+
+  const memoPlayer = useMemo(()=>{
+    return players.map((player,index)=>{
+      let pos:number|undefined;
+      if(gameState===GameStateEnum.END){
+        if(index===0 || index ===1 || index==2){
+          pos = index+1;
+        }
+      }
+      return (
+        <Avatar name={player.name}
+          pos={pos}
+          id={player.id}
+          key ={player.id}
+          score={player.score}
+          src={player.avatar || avatarImage  }
+        
+        />
+      )
+    })
+  },[gameState,players])
+
+
   return (
-    <div>Player</div>
+    <div className='fixed h-1/5 w-full bottom-0 overflow-x-auto flex overflow-y-hidden items-baseline justify-center'>{memoPlayer}</div>
   )
 }
 
-export default Player
+export default observer(Player)
